ab756e50dd2c5bdf7f2a97c429b3850fb0dc3ed9
saraemad32 <sara2810200461120041975@gmail.com>
2025-08-31 15:05:38 +0300

    Add Flutter project

---
+++ flutter/BackEnd/Ecommerce.Entities/Models/CartItem.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.Entities.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid CartId { get; set; }
+
+        [ForeignKey(nameof(CartId))]
+        public Cart Cart { get; set; }
+
+        [Required]
+        public Guid ProductId { get; set; }
+
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ flutter/BackEnd/Ecommerce.Entities/Models/Order.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Ecommerce.Entities.Models.Auth.Identity;
+using Ecommerce.Utilities.Enums;
+
+namespace Ecommerce.Entities.Models
+{
+    public class Order
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string BuyerId { get; set; }
+
+        [ForeignKey(nameof(BuyerId))]
+        public User Buyer { get; set; }
+
+        // [Required]
+        // public string SellerId { get; set; }
+
+        // [ForeignKey(nameof(SellerId))]
+        // public Seller Seller { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+        public decimal TotalPrice { get; set; }
+
+        public string ShippingAddress { get; set; }
+        public decimal ShippingPrice { get; set; }
+       
+        public string TrackingNumber { get; set; }
+
+        public string CourierService { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        public DateTime? ShippedDate { get; set; }
+
+        public DateTime? DeliveredDate { get; set; }
+
+        public DateTime? CancelledDate { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}

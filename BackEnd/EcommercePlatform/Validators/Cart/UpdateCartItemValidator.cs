1a9dd484b6c480e3b9a1b1fc7e60c914aec6f033
dotsy12 <dotse159@gmail.com>
2025-09-01 21:46:21 +0300

    add update features in cart

---
+++ BackEnd/EcommercePlatform/Validators/Cart/UpdateCartItemValidator.cs
@@ -0,0 +1,17 @@
+﻿using Ecommerce.Entities.DTO.Cart;
+
+using FluentValidation;
+
+namespace Ecommerce.Business.Validators.Cart
+{
+	public class UpdateCartItemValidator : AbstractValidator<UpdateCartRequest>
+	{
+		public UpdateCartItemValidator()
+		{
+			RuleFor(x => x.Quantity)
+			   .NotNull().WithMessage("Quantity is required.")
+			   .GreaterThan(0).WithMessage("Quantity must be greater than 0.")
+			   .LessThanOrEqualTo(1000).WithMessage("Quantity is too large."); // عدّل الحد لو حابب
+		}
+	}
+}

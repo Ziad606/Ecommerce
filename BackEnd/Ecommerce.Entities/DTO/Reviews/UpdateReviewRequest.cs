ecf4cd852a2341e5f399f4b34dc2eb720a37a32a
Assem <assem654321@gmail.com>
2025-09-03 13:45:29 +0300

    feat(Review): Add Complete  Review Module Logic & Endpoints Including: - Review DTOs for exposing Review Fields - Review Services that define reviews essential methods - Review Validators to validate user input - Review Controller that define Review Endpoints

---
+++ BackEnd/Ecommerce.Entities/DTO/Reviews/UpdateReviewRequest.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Utilities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Entities.DTO.Reviews
+{
+    public class UpdateReviewRequest
+    {
+        public ReviewRating Rating { get; set; }
+        public string Comment { get; set; }
+        public bool Confirm { get; set; }
+    }
+}
